8eb2673c3fe8fc7c96a13f84fac9bee133e882e7
tadataku-lab <tadataku.lab@gmail.com>
2019-08-06 14:27:43 +0900

    fet: use matter.js

--- puppy/client2/src/components/Main/Main.tsx
+++ puppy/client2/src/components/Main/Main.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { Engine, World, Bodies } from 'matter-js';
 
 const Main: React.FC = () => {
+  useEffect(() => {
+    const engine = Engine.create(document.getElementById('puppy-screen')!);
+
+    World.add(engine.world, [
+      Bodies.rectangle(320, 460, 480, 20, {
+        isStatic: true, //固定する
+        render: {
+          fillStyle: '#977559', // 塗りつぶす色: CSSの記述法で指定
+          strokeStyle: 'rgba(0, 0, 0, 0)', // 線の色: CSSの記述法で指定
+          lineWidth: 0,
+        },
+      }),
+    ]);
+
+    Engine.run(engine);
+  });
+
   return (
     <div className="Main">
       <Row>
