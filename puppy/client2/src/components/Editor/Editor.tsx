44d996f27fa6e604364e5901cba68634ce2c3b35
tadataku-lab <tadataku.lab@gmail.com>
2019-08-10 15:59:27 +0900

    fet: impl zenkaku checker

--- puppy/client2/src/components/Editor/Editor.tsx
+++ puppy/client2/src/components/Editor/Editor.tsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import MonacoEditor from 'react-monaco-editor';
+import * as monacoEditor from 'monaco-editor'; // eslint-disable-line no-unused-vars
+import './Editor.css';
+
+type CodeEditor = monacoEditor.editor.IStandaloneCodeEditor;
+
+const zenkaku =
+  '[！　”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［＼￥］＾＿‘｛｜｝～￣' +
+  'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ' +
+  'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ' +
+  '１２３４５６７８９０' +
+  '｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾉﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ]';
 
 const Editor: React.FC = () => {
   const [code, setCode] = useState('');
   const [width, setWidth] = useState(500);
   const [height, setHeight] = useState(500);
+  const [codeEditor, setCodeEditor]: [
+    CodeEditor | null,
+    React.Dispatch<React.SetStateAction<CodeEditor | null>>
+  ] = useState(null as CodeEditor | null);
+  const [decoration, setDecoration]: [
+    string[],
+    React.Dispatch<React.SetStateAction<string[]>>
+  ] = useState([] as string[]);
 
-  const options = {
+  const editorOptions = {
     selectOnLineNumbers: true,
     fontSize: 30,
     wordWrap: 'on' as 'on',
@@ -27,15 +46,35 @@ const Editor: React.FC = () => {
     setHeight(document.getElementById('right-col')!.clientHeight);
   });
 
+  const codeOnChange = (code: string) => {
+    setCode(code);
+    if (codeEditor) {
+      const zenkakuRanges = codeEditor
+        .getModel()!
+        .findMatches(zenkaku, true, true, false, null, false);
+      const decos: monacoEditor.editor.IModelDeltaDecoration[] = zenkakuRanges.map(
+        (match: monacoEditor.editor.FindMatch) => ({
+          range: match.range,
+          options: { inlineClassName: 'zenkakuClass' },
+        })
+      );
+      setDecoration(codeEditor.deltaDecorations(decoration, decos));
+    }
+  };
+
+  const editorDidMount = (editor: CodeEditor) => {
+    setCodeEditor(editor);
+  };
+
   return (
     <MonacoEditor
       width={width}
       height={height}
       language="python"
       value={code}
-      options={options}
-      onChange={setCode}
-      editorDidMount={editor => editor.focus()}
+      options={editorOptions}
+      onChange={codeOnChange}
+      editorDidMount={editorDidMount}
     />
   );
 };
